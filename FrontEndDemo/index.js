31a7abdb05e31b500c962c63043aa23048b9bdf3
Agnieszka Kurek <agniechakurek@gmail.com>
2016-01-25 14:50:17 +0100

    add hidden messages

---
+++ FrontEndDemo/index.js
@@ -0,0 +1,18 @@
+﻿var msg = "hello JavaScript";
+console.log(msg);
+
+var resultsDiv = document.getElementById("results");
+resultsDiv.innerHTML = "<p>This is from Java Script</p>";
+
+console.log("msg is " + typeof (msg));
+console.log("resultsDiv is" + typeof (resultsDiv));
+
+var none;
+console.log("none is" + typeof (none));
+
+var aNumber = 0;
+console.log("aNumber is" + typeof (aNumber));
+ 
+var trueFalse = true;
+console.log("trueFalse is" + typeof (trueFalse));
+     
