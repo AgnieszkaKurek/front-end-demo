805b2bc22f1ff75c6c932f90b6372bf6af9b07b4
Agnieszka Kurek <agniechakurek@gmail.com>
2016-01-26 13:16:54 +0100

    Add new objects

--- FrontEndDemo/index.js
+++ FrontEndDemo/index.js
@@ -4,69 +4,49 @@ console.log(msg);
 var resultsDiv = document.getElementById("results");
 resultsDiv.innerHTML = "<p>This is from Java Script</p>";
 
-console.log("msg is " + typeof (msg));
-console.log("resultsDiv is" + typeof (resultsDiv));
-
-var none;
-console.log("none is" + typeof (none));
-
-var aNumber = 10;
-console.log("aNumber is" + typeof (aNumber));
- 
-var trueFalse = true;
-console.log("trueFalse is" + typeof (trueFalse));
-
-//msgs = "this shouldn't work";
-
-if (!none == undefined) {
-    console.log("none is undefined");
-}
 
 
-if (aNumber == "10") {
-    console.log("10 is 10")
-}
-
-//function showMsg(msg) {
-   // console.log("showMsg:" + msg);
-//}
-function showMsg(msg, more) {
-    if (more) {
-        console.log("showMsg+" + msg + more)
-    } else {
-        console.log("showMsg+" + msg);
+var result = {
+    name: "jQuery",
+    language: "JavaScript",
+    score: 4.5,
+    showLog: function () {
+    },
+    owner: {
+        login: "agnieszkakurek",
+        id:123456
     }
-}
+};
 
-showMsg("some information");
-showMsg("some information", "and even more");
+result.phoneNumber = "123-456-7890";
 
-var showIt = function (msg) {
-    console.log(msg);
-}
+console.log(result.phoneNumber);
 
-showMsg("Some message");
+var results = [{
+    name: "jQuery",
+    language: "JavaScript",
+    score: 4.5,
+    showLog: function () {
 
-function showItThen(msg, callback) {
-    showIt(msg);
-    callback();
-}
+    },
+    owner: {
+        login: "agnieszkakurek",
+        id: 123456
+    }, {
+    name: "jQuery UI",
+    language: "JavaScript",
+    score: 3.5,
+    showLog: function () {
 
-showItThen("showItThen called", function () {
-    console.log("collback called");
-});
+    },
+    owner: {
+        login: "agnieszkakurek",
+        id: 123456
+    }
+},];
 
-var inGlobal = true;
+results.push(result);
+results.push({
+    name:"dummy projeect"
+})
 
-function testMe() {
-    console.log("testMe():" + inGlobal);
-    var someMsg = "some Message";
-    console.log("testMe():" + someMsg);
-    showItThen("with Closure", function () {
-        showIt("testMe With Closure():" + someMsg);
-    });
-}
-//console.log("global:" + someMsg);
-testMe();
-
-     
