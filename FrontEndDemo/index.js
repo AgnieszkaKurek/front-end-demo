8efd7b087893b73b9c0c67e36f41dacb8a1385da
Agnieszka Kurek <agniechakurek@gmail.com>
2016-01-27 11:15:35 +0100

    adding function "ready"

--- FrontEndDemo/index.js
+++ FrontEndDemo/index.js
@@ -1,76 +1,80 @@
-﻿//var msg = "hello JavaScript";
-//console.log(msg);
-
-//var resultsDiv = document.getElementById("results");
-//resultsDiv.innerHTML = "<p>This is from Java Script</p>";
-
-var resultList = $("#resultList");
-resultList.text("This is from jQuery");
-
-var toggleButton = $("#toggleButton");
-toggleButton.on("click", function () {
-    resultList.toggle(500);
-
-    if (toggleButton.text() == "Hide") toggleButton.text("Show");
-    else toggleButton.text("Hide");
-
-});
+﻿$(document).ready(function () {
 
 
+    //var msg = "hello JavaScript";
+    //console.log(msg);
 
-//var result = {
-//name: "jQuery",
-//language: "JavaScript",
-//score: 4.5,
-//showLog: function () {
-// },
-//owner: {
-// login: "agnieszkakurek",
-// id:123456
-//}
-//};
+    //var resultsDiv = document.getElementById("results");
+    //resultsDiv.innerHTML = "<p>This is from Java Script</p>";
 
-//result.phoneNumber = "123-456-7890";
+    var resultList = $("#resultList");
+    resultList.text("This is from jQuery");
 
-//console.log(result.phoneNumber);
+    var toggleButton = $("#toggleButton");
+    toggleButton.on("click", function () {
+        resultList.toggle(500);
 
-//var results = [
-   // {
-       // name: "jQuery",
-       // language: "JavaScript",
-        //score: 4.5,
-        //showLog: function () {
+        if (toggleButton.text() == "Hide") toggleButton.text("Show");
+        else toggleButton.text("Hide");
 
-       // },
-        //owner: {
-           // login: "agnieszkakurek",
-           // id: 123456
-       // }
+    });
+
+
+
+    //var result = {
+    //name: "jQuery",
+    //language: "JavaScript",
+    //score: 4.5,
+    //showLog: function () {
+    // },
+    //owner: {
+    // login: "agnieszkakurek",
+    // id:123456
+    //}
+    //};
+
+    //result.phoneNumber = "123-456-7890";
+
+    //console.log(result.phoneNumber);
+
+    //var results = [
+    // {
+    // name: "jQuery",
+    // language: "JavaScript",
+    //score: 4.5,
+    //showLog: function () {
+
+    // },
+    //owner: {
+    // login: "agnieszkakurek",
+    // id: 123456
+    // }
     //},
     //{
-       // name: "jQuery UI",
-        //language: "JavaScript",
-        //score: 3.5,
-        //showLog: function () {
+    // name: "jQuery UI",
+    //language: "JavaScript",
+    //score: 3.5,
+    //showLog: function () {
 
-        //},
-        //owner: {
-           // login: "agnieszkakurek",
-            //id: 123456
-       // }
     //},
-//];
+    //owner: {
+    // login: "agnieszkakurek",
+    //id: 123456
+    // }
+    //},
+    //];
 
-//for (var x = 0; x < results.length; x++) {
-   // var result = results[x];
+    //for (var x = 0; x < results.length; x++) {
+    // var result = results[x];
     //if (result.score < 4) continue-9+60;
     //console.log(result.name);
-//}
-//console.log(results.length);
-//console.log(results[0].name);
+    //}
+    //console.log(results.length);
+    //console.log(results[0].name);
 
-//results.push(result);
-//results.push({
-//name:"dummy projeect"
-//})
+    //results.push(result);
+    //results.push({
+    //name:"dummy projeect"
+    //})
 
+});
