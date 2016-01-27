dd41cec6087752476ec07ec77956673203ff1142
Agnieszka Kurek <agniechakurek@gmail.com>
2016-01-27 11:09:44 +0100

    adding features to change the text on the button on click

--- FrontEndDemo/index.js
+++ FrontEndDemo/index.js
@@ -10,6 +10,10 @@ resultList.text("This is from jQuery");
 var toggleButton = $("#toggleButton");
 toggleButton.on("click", function () {
     resultList.toggle(500);
+
+    if (toggleButton.text() == "Hide") toggleButton.text("Show");
+    else toggleButton.text("Hide");
+
 });
 
 
