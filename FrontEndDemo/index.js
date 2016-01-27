3f79c6332048a02a70e5590571af9e0231fb5bae
Agnieszka Kurek <agniechakurek@gmail.com>
2016-01-27 09:43:02 +0100

    add new div "resultList"

--- FrontEndDemo/index.js
+++ FrontEndDemo/index.js
@@ -1,8 +1,9 @@
-﻿var msg = "hello JavaScript";
-console.log(msg);
+﻿//var msg = "hello JavaScript";
+//console.log(msg);
 
-var resultsDiv = document.getElementById("results");
-resultsDiv.innerHTML = "<p>This is from Java Script</p>";
+//var resultsDiv = document.getElementById("results");
+//resultsDiv.innerHTML = "<p>This is from Java Script</p>";
+var resultList = jQuery("resultList")
 
 
 
@@ -22,38 +23,38 @@ resultsDiv.innerHTML = "<p>This is from Java Script</p>";
 
 //console.log(result.phoneNumber);
 
-var results = [
-    {
-        name: "jQuery",
-        language: "JavaScript",
-        score: 4.5,
-        showLog: function () {
+//var results = [
+   // {
+       // name: "jQuery",
+       // language: "JavaScript",
+        //score: 4.5,
+        //showLog: function () {
 
-        },
-        owner: {
-            login: "agnieszkakurek",
-            id: 123456
-        }
-    },
-    {
-        name: "jQuery UI",
-        language: "JavaScript",
-        score: 3.5,
-        showLog: function () {
+       // },
+        //owner: {
+           // login: "agnieszkakurek",
+           // id: 123456
+       // }
+    //},
+    //{
+       // name: "jQuery UI",
+        //language: "JavaScript",
+        //score: 3.5,
+        //showLog: function () {
 
-        },
-        owner: {
-            login: "agnieszkakurek",
-            id: 123456
-        }
-    },
-];
+        //},
+        //owner: {
+           // login: "agnieszkakurek",
+            //id: 123456
+       // }
+    //},
+//];
 
-for (var x = 0; x < results.length; x++) {
-    var result = results[x];
-    if (result.score < 4) continue-9+60;
-    console.log(result.name);
-}
+//for (var x = 0; x < results.length; x++) {
+   // var result = results[x];
+    //if (result.score < 4) continue-9+60;
+    //console.log(result.name);
+//}
 //console.log(results.length);
 //console.log(results[0].name);
 
